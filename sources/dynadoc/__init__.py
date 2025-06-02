6d3750e5fa7588585d9ac25fd27e08476eb8d79b
Eric McDonald <emcd@users.noreply.github.com>
2025-06-02 16:41:05 -0700

    Version: 1.2

--- sources/dynadoc/__init__.py
+++ sources/dynadoc/__init__.py
@@ -37,7 +37,7 @@ from .userapi import *
 
 
 __version__: __.typx.Annotated[ str, Visibilities.Reveal ]
-__version__ = '1.2rc0'
+__version__ = '1.2'
 
 
 def _notify( level: NotificationLevels, message: str ) -> None:
